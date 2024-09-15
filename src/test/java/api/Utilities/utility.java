267eae2210818fe9cb16a5481ac02e7e7896d3f9
Draj19071997 <draj.shet45@gmail.com>
2024-09-15 23:45:33 +0530

    added comment for property class in utility package

--- src/test/java/api/Utilities/utility.java
+++ src/test/java/api/Utilities/utility.java
@@ -3,7 +3,7 @@ package api.Utilities;
 import java.util.ResourceBundle;
 
 public class utility {
-	
+	//Ulitity to load property file
 	public static ResourceBundle getURL()
 	{
 		ResourceBundle bundle=ResourceBundle.getBundle("Routes"); //Load the URL from Properties files
